d0c7a3e1dd0ad088023a6ae81f7a2cd0de702f9f
PacoAlexM <pako_ale_martell@hotmail.com>
2025-03-25 22:45:48 -0600

    Se agrega funcion para activar el dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import MoonIcon from './MoonIcon';
+import SunIcon from './SunIcon';
 
-const Header = () => (
-    <header className="container mx-auto px-4 pt-8">
-        <div className="flex justify-between">
-            <h1 className="uppercase text-white text-2xl font-bold tracking-[0.5em]">Todo</h1>
-            <button><MoonIcon fill="#fff" /></button>
-        </div>
-    </header>
-);
+const Header = () => {
+    const [dark, setDark] = useState(false)
+
+    useEffect(() => {
+        if (dark) {
+            document.documentElement.classList.add('dark');
+            localStorage.theme = 'dark';
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = 'light';
+        }
+    }, [dark]);
+
+    return (
+        <header className="container mx-auto px-4 pt-8">
+            <div className="flex justify-between">
+                <h1 className="uppercase text-white text-2xl font-bold tracking-[0.5em]">Todo</h1>
+                <button onClick={ () => setDark(!dark) }>
+                    { dark ? <SunIcon fill="#fff" /> : <MoonIcon fill="#fff" /> }
+                </button>
+            </div>
+        </header>
+    )
+};
 
 export default Header;
